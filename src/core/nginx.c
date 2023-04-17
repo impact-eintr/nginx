1e74a3d7cb392be01cdd54c8f5c39ef09037e30a
impact-eintr <yixingwei4@gmail.com>
2023-04-17 14:12:39 +0800

    firtst commit for nginx

---
+++ src/core/nginx.c
@@ -0,0 +1,8 @@
+#include "nginx.h"
+
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+  printf("welcome to nginx\n");
+}
---
+++ src/core/nginx.h
@@ -0,0 +1,6 @@
+#ifndef NGINX_H_
+#define NGINX_H_
+
+
+
+#endif // NGINX_H_
